f498e040973e1e00438e61024112280c90ee2d52
zhiwen95 <wei.zhiwen@outlook.com>
2023-09-07 13:49:32 +0800

    chore: copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} 阿拉喵, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} 阿拉喵. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
