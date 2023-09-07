9d834c0f31a90ae53c70fc917977aa7fe237ae20
zhiwen95 <wei.zhiwen@outlook.com>
2023-09-07 11:33:16 +0800

    first commit

---
+++ src/theme/BlogPostPaginator/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BlogPostPaginator from "@theme-original/BlogPostPaginator";
+import Twikoo from "@site/src/components/Twikoo";
+
+export default function BlogPostPaginatorWrapper(props) {
+  return (
+    <>
+      <BlogPostPaginator {...props} />
+      <hr />
+      <div>
+        <Twikoo />
+      </div>
+    </>
+  );
+}
