e3f79993022a1f5c19511612bffda27e7acceea6
zhiwen95 <wei.zhiwen@outlook.com>
2023-09-08 15:11:29 +0800

    feat: gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,6 +55,13 @@ const config = {
           customCss: require.resolve('./src/css/custom.css'),
         },
       }),
+      '@docusaurus/preset-classic',
+      {
+        gtag: {
+          trackingID: 'G-L81BHCQ9YW',
+          anonymizeIP: true,
+        },
+      },
     ],
   ],
 
